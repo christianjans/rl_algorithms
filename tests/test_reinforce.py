0c6c75c60fc39bdb5259430ea6f7c029217a2c70
Christian Jans <cjjans@ualberta.ca>
2021-08-25 16:04:20 -0600

    Add initial Reinforce test

---
+++ tests/test_reinforce.py
@@ -0,0 +1,29 @@
+from tests.common import run_agent_in_environment
+import unittest
+
+import gym
+
+from reinforce.agent import Reinforce, ReinforceConfig
+
+
+class ReinforceTest(unittest.TestCase):
+  def test_reinforce_discrete_action(self):
+    env = gym.make("CartPole-v0")
+
+    EPISODES_ALLOWED = 2000
+    MAX_RETURN = 200.0
+
+    reinforce_config = ReinforceConfig(nn_width=32,
+                                       nn_depth=4,
+                                       optimize_rate=10,
+                                       learning_rate=0.001,
+                                       gamma=0.999,
+                                       device="cpu",
+                                       observation_shape=env.observation_space.shape,
+                                       action_size=env.action_space.n)
+    reinforce_agent = Reinforce(reinforce_config)
+
+    completed, info = run_agent_in_environment(
+        reinforce_agent, env, EPISODES_ALLOWED, MAX_RETURN)
+
+    self.assertTrue(completed, msg="\n".join(info))
